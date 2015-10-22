6cc2076348c8a4381c9d9908ab31b751849ca2c0
nipakoo <nipakoo92@hotmail.com>
2015-10-22 20:27:01 +0300

    small fixes

--- browser_plugin/src/chrome_extension/gaze_matcher.js
+++ browser_plugin/src/chrome_extension/gaze_matcher.js
@@ -1,3 +1,5 @@
+// TODO coordinates are currently count by using the percentage of whole screen and size of the browsers view, need to move coordinate starting point to browser view starting point
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 	var elements = document.elementsFromPoint(request.gazeX * window.screen.availWidth, request.gazeY * window.screen.availHeight);
